109d90b72b6d13952109db86618ef0d9a4a1cb8f
SeungwonLee <97718938+Heisnotanimposter@users.noreply.github.com>
2024-03-28 16:57:55 +0900

    1
    1

--- README.md
+++ README.md
@@ -5,3 +5,31 @@ Open Source Project
 한국산업인력공단 자격증 리스트
 https://www.data.go.kr/iim/api/selectAPIAcountView.do
 ./Bumblebee/dataset/CBTsubjectlist_20230308.csv
+
+
+요청변수(Request Parameter)
+
+
+항목명(국문)	항목명(영문)	항목크기	항목구분	샘플데이터	항목설명
+인증키	serviceKey	100	필	공공데이터포털에서 받은 인증키	발급받은 인증키
+등급코드	grdCd	2	필	10	등급코드
+기준년도	baseYY	4	필	2020	기준연도
+페이지 번호	pageNo	5	옵	1	페이지 번호
+페이지당 데이터 수	numOfRows	5	옵	1	페이지당 데이터 수
+출력결과(Response Element)
+
+
+항목명(국문)	항목명(영문)	항목크기	항목구분	샘플데이터	항목설명
+합격자수	examPassCnt	10	필	2	합격자수
+필/실기 구분	examTypCcd	30	필	필기	필/실기 구분
+등급명	grdNm	100	필	기술사	등급명
+시행회차	implSeq	200	필	108	시행회차
+시행년도	implYy	30	필	2016	시행년도
+종목명	jmFldNm	200	필	가스기술사	종목명
+합격률	passRate	4	필	10.5%	합격률
+응시자수	recptNoCnt	10	필	19	응시자수
+페이지당 데이터 수	numOfRows	5	필	10	페이지당 데이터 수
+페이지 번호	pageNo	5	필	1	페이지 번호
+데이터 총 개수	totalCount	10	필	10000	페이지 총 개수
+결과코드	resultCode	2	필	00	결과코드
+결좌메세지	resultMsg	50	필	NORMAL SERVICE	결좌메세지
---
+++ dataset/api_request_sample.java
@@ -0,0 +1,39 @@
+/* Java 1.8 샘플 코드 */
+/* https://www.data.go.kr/data/15089380/openapi.do
+
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class ApiExplorer {
+    public static void main(String[] args) throws IOException {
+        StringBuilder urlBuilder = new StringBuilder("http://openapi.q-net.or.kr/api/service/rest/InquiryQualPassRateSVC/getList"); /*URL*/
+        urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=서비스키"); /*Service Key*/
+        urlBuilder.append("&" + URLEncoder.encode("grdCd","UTF-8") + "=" + URLEncoder.encode("10", "UTF-8")); /*등급코드*/
+        urlBuilder.append("&" + URLEncoder.encode("baseYY","UTF-8") + "=" + URLEncoder.encode("2020", "UTF-8")); /*기준연도*/
+        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지 번호*/
+        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지당 데이터 수*/
+        URL url = new URL(urlBuilder.toString());
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Content-type", "application/json");
+        System.out.println("Response code: " + conn.getResponseCode());
+        BufferedReader rd;
+        if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        } else {
+            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+        }
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = rd.readLine()) != null) {
+            sb.append(line);
+        }
+        rd.close();
+        conn.disconnect();
+        System.out.println(sb.toString());
+    }
+}
